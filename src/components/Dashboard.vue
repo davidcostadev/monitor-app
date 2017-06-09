c06efc6e4537b473c6166f7b6e877abab2b176f8
David Costa <novam3@iMac-de-David.local>
2017-06-09 10:41:31 -0300

    adicionando clientes

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,19 @@
 <template>
 
-  <div>
+  <div class="numbers">
     <div v-for="number in numbers" v-bind:class="`card-number ${numbers_status[number.number]}`">
+
       <div class="card-number-content">
+        <div class="card-number-machine">
+          {{number.maquine}}
+        </div>
         {{numbers_status[number.number]}}
       </div>
-      <div class="card-number-footer">
-        {{number.number}}
+      <div class="card-number-name">
         {{number.name}}
+      </div>
+      <div class="card-number-number">
+        {{number.number}}
       </div>
     </div>
 
@@ -64,15 +70,53 @@
 </script>
 
 <style>
-  .card-number.off,
-  .card-number.error {
+  .numbers {
+    display: flex;
+    flex-flow: row wrap;
+    text-align: center;
+    align-items: stretch;
+  }
+  .card-number{
+    margin: 5px 5px;
+    flex: 1 10%;
+    min-width: 250px;
+  }
+  .card-number .card-number-content{
+    transition: all 1s;
+  }
+  .card-number.off .card-number-content,
+  .card-number.error .card-number-content{
     background: red;
   }
-  .card-number.waiting {
-    background: orange;
-  }
-  .card-number.ok {
+  .card-number.esperando .card-number-content{
     background: green;
+  }
+  .card-number.ok .card-number-content{
+    background: greenyellow;
+    color: black;
+  }
+  .card-number-content{
+    font-size: 46px;
+    padding: 20px 15px;
+  }
+  .card-number-machine {
+    padding: 5px 15px;
+    margin: -20px -15px 20px;
+    font-size: 12px;
+    background: rgba(0, 0, 0, 0.5);
+  }
+  .card-number-name {
+    padding: 5px 15px;
+    background: #252525;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .card-number-number {
+    padding: 0 15px 5px;
+    background: #252525;
+    font-size: 12px;
+    color: gray;
   }
 </style>
 
